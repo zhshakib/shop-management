5c24e30b50b8238dc7d2184af66f32c431f714dd
zhshakib <zhshakib@gmail.com>
2022-01-14 15:04:00 +0600

    Initial Commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h2>Todo list here!</h2>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">{{ todo.task }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+const axios = require('axios');
+
+export default {
+  data() {
+    return {
+      todos: [],
+    };
+  },
+  created() {
+    axios
+      .get('http://localhost:8000/todo/')
+      .then((response) => {
+        this.todos = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+};
+</script>
+
+<style>
+</style>
